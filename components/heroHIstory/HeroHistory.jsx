8632e6ff5660a9ea65abef0ca247f7944684484d
victor <103121630+victor-914@users.noreply.github.com>
2023-11-08 08:58:37 +0100

    new_status

--- components/heroHIstory/HeroHistory.jsx
+++ components/heroHIstory/HeroHistory.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 function HeroHistory() {
-  return <StyledHeroHistory>HeroHistory</StyledHeroHistory>;
+  return (
+    <StyledHeroHistory>
+      <main className="">
+        <header></header>
+        <div>
+
+        </div>
+      </main>
+    </StyledHeroHistory>
+  );
 }
 
 export default HeroHistory;
